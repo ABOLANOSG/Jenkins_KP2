a62c9ed6b7f1c86430d780b0d02466675cb99c46
ABOLANOSG <a.bolanos@a1qa.com>
2024-07-03 08:34:26 -0600

    set configurations to mentor survey excercise

---
+++ configs/chrome.mocha.conf.js
@@ -0,0 +1,23 @@
+import { downloadDir, mainConfig } from "../framework/configs/main.wdio.conf.js";
+
+
+export const config = {
+    ...mainConfig,
+    ...{
+        specs: [
+            '../test/specs/**/*.js'
+        ],
+        capabilities: [
+            {
+                browserName: "chrome",
+                browserVersion: '126.0.6478.127',
+                "goog:chromeOptions": {
+                    prefs: {
+                        "download.default_directory": downloadDir,
+                        'intl.accept_languages': 'en,en_US'
+                    }
+                },
+            },
+        ],
+    },
+};
