ee6d562bacfcfeccd42a761c9c6cfb368b327f13
Andrea Bolanos <andrea.bolanos@kandasoft.com>
2025-07-21 15:21:57 -0600

    changes added to config file to accept parameterization

--- configs/chrome.mocha.conf.js
+++ configs/chrome.mocha.conf.js
@@ -1,5 +1,6 @@
 import { downloadDir, mainConfig } from "../framework/configs/main.wdio.conf.js";
 
+const browserName = process.env.BROWSER || "chrome"
 
 export const config = {
     ...mainConfig,
@@ -9,7 +10,7 @@ export const config = {
         ],
         capabilities: [
             {
-                browserName: "chrome",
+                browserName: browserName,
                 browserVersion: '126.0.6478.127',
                 "goog:chromeOptions": {
                     args: ['--disable-blink-features=Autofill'],
@@ -18,6 +19,15 @@ export const config = {
                         'intl.accept_languages': 'en,en_US'
                     }
                 },
+                ...(browserName === "edge" && {
+                    "ms:edgeOptions": {
+                        args: ['--disable-blink-features=Autofill'],
+                        prefs: {
+                            "download.default_directory": downloadDir,
+                            'intl.accept_languages': 'en,en_US'
+                        }
+                    }
+                })
             },
         ],
     },
