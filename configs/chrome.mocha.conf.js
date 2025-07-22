a411e583787d6085d181ead0a4e005d51793fa14
Andrea Bolanos <andrea.bolanos@kandasoft.com>
2025-07-21 18:18:59 -0600

    edge configuration

--- configs/chrome.mocha.conf.js
+++ configs/chrome.mocha.conf.js
@@ -1,6 +1,6 @@
 import { downloadDir, mainConfig } from "../framework/configs/main.wdio.conf.js";
 
-const browserName = process.env.BROWSER || "chrome"
+const browserName = process.env.BROWSER;
 
 export const config = {
     ...mainConfig,
@@ -9,25 +9,9 @@ export const config = {
             '../test/specs/**/*.js'
         ],
         capabilities: [
-            browserName === "edge"
-                ? {
+            {
                     browserName: "MicrosoftEdge",
                     "ms:edgeOptions": {
-                        args: [
-                            '--headless',
-                            '--disable-gpu',
-                            '--disable-blink-features=Autofill',
-                            '--window-size=1920,1080'
-                        ],
-                        prefs: {
-                            "download.default_directory": downloadDir,
-                            'intl.accept_languages': 'en,en_US'
-                        }
-                    }
-                }
-                : {
-                    browserName: "chrome",
-                    "goog:chromeOptions": {
                         args: [
                             '--headless',
                             '--disable-gpu',
